0b5ca3e57861ce87c9dfbea896363dda2760c7fc
Vitaliy Pyatnikov <vpyatnikov@gmail.com>
2019-07-07 19:12:13 +0300

    add: Initial project setup

---
+++ YandexCourse/YandexCourse/Note.swift
@@ -0,0 +1,51 @@
+//
+//  Note.swift
+//  YandexCourse
+//
+//  Created by Vitaliy Pyatnikov on 01.07.2019.
+//  Copyright © 2019 Vitaliy Pyatnikov. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - Note
+
+struct Note {
+
+    // MARK: - Properties
+
+    let title: String
+    let content: String
+    let importance: Importance
+    let uid: String
+    let color: UIColor
+    let selfDestructionDate: Date?
+
+    // MARK: - Initialisation
+
+    init(title: String,
+         content: String,
+         importance: Importance,
+         uid: String = UUID().uuidString,
+         color: UIColor = .white,
+         selfDestructionDate: Date? = nil) {
+        self.title = title
+        self.content = content
+        self.importance = importance
+        self.uid = uid
+        self.color = color
+        self.selfDestructionDate = selfDestructionDate
+    }
+}
+
+
+// MARK: - Importance
+
+enum Importance {
+
+    // MARK: - Cases
+
+    case unimportant
+    case usual
+    case important
+}
---
+++ YandexCourse/YandexCourse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YandexCourse
+//
+//  Created by Vitaliy Pyatnikov on 01.07.2019.
+//  Copyright © 2019 Vitaliy Pyatnikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
