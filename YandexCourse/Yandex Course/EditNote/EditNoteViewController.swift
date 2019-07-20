db6b411887f4d6a0e4d8a8ac148439262aa08a50
Vitaliy Pyatnikov <vpyatnikov@gmail.com>
2019-07-20 12:49:44 +0300

    add: EditNote scene

---
+++ YandexCourse/Yandex Course/EditNote/EditNoteViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EditNoteViewController.swift
+//  Yandex Course
+//
+//  Created by Vitaliy Pyatnikov on 20.07.2019.
+//  Copyright © 2019 Vitaliy Pyatnikov. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - EditNoteViewController
+
+final class EditNoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- YandexCourse/Yandex Course/ViewController.swift
+++ YandexCourse/Yandex Course/ViewController.swift
@@ -8,13 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBAction func editNoteAction(_ sender: Any) {
+        performSegue(withIdentifier: "EditNoteSegue", sender: self)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "EditNoteSegue" {
+            guard let _ = segue.destination as? EditNoteViewController else {
+                return
+            }
+        }
+    }
 }
 
