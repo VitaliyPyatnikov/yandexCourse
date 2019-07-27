d528c02c3868c64cb39d13d098d48fdadec00120
Vitaliy Pyatnikov <vpyatnikov@gmail.com>
2019-07-27 17:56:49 +0300

    add: colorSelectionView

--- YandexCourse/Yandex Course/Screens/ColorPicker/ColorPickerViewController.swift
+++ YandexCourse/Yandex Course/Screens/ColorPicker/ColorPickerViewController.swift
@@ -31,11 +31,15 @@ final class ColorPickerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         colorizeImageView()
+        setupColorSelectionView()
     }
 
     // MARK: - Private
     /// Identifies spectre lines direction
     private var hueHorizontal = true
+    private var colorSelectionView: UIView = UIView()
+    private var selectionViewConstraintX: NSLayoutConstraint = NSLayoutConstraint()
+    private var selectionViewConstraintY: NSLayoutConstraint = NSLayoutConstraint()
 
     // MARK: - Color Image view
 
@@ -67,5 +71,59 @@ final class ColorPickerViewController: UIViewController {
         let hue = hueHorizontal ? point.x / size.width : point.y / size.height
         let saturation = hueHorizontal ? point.y / size.height : point.x / size.width
         return (max (0, min(1, hue)), 1 - max(0, min(1, saturation)))
+    }
+
+    // MARK: - Color selection view
+
+    private func setupColorSelectionView() {
+        colorSelectionView.translatesAutoresizingMaskIntoConstraints = false
+        colorImageView.addSubview(colorSelectionView)
+        // setup preview
+        colorSelectionView.layer.masksToBounds = true
+        colorSelectionView.layer.borderWidth = 4.0
+        colorSelectionView.layer.borderColor = UIColor.gray.cgColor
+        colorSelectionView.layer.cornerRadius = 10.0
+        colorSelectionView.alpha = 1.0
+        colorSelectionView.backgroundColor = .clear
+
+        setupConstraintsForSelectionView()
+    }
+    private func setupConstraintsForSelectionView() {
+        selectionViewConstraintX = NSLayoutConstraint(item: colorSelectionView,
+                                                      attribute: .centerX,
+                                                      relatedBy: .equal,
+                                                      toItem: colorImageView,
+                                                      attribute: .left,
+                                                      multiplier: 1,
+                                                      constant: 0)
+        selectionViewConstraintY = NSLayoutConstraint(item: colorSelectionView,
+                                                      attribute: .centerY,
+                                                      relatedBy: .equal,
+                                                      toItem: colorImageView,
+                                                      attribute: .top,
+                                                      multiplier: 1,
+                                                      constant: 0)
+        let widthConstraint = NSLayoutConstraint(item: colorSelectionView,
+                                                 attribute: NSLayoutConstraint.Attribute.width,
+                                                 relatedBy: NSLayoutConstraint.Relation.equal,
+                                                 toItem: nil,
+                                                 attribute: .notAnAttribute,
+                                                 multiplier: 1,
+                                                 constant: 20)
+        let heightConstraint = NSLayoutConstraint(item: colorSelectionView,
+                                                  attribute: .height,
+                                                  relatedBy: .equal,
+                                                  toItem: nil,
+                                                  attribute: .notAnAttribute,
+                                                  multiplier: 1,
+                                                  constant: 20)
+        NSLayoutConstraint.activate([selectionViewConstraintX,
+                                     selectionViewConstraintY,
+                                     widthConstraint,
+                                     heightConstraint])
+    }
+    private func updatePositionOfSelectorView(withPoint point: CGPoint) {
+        selectionViewConstraintX.constant = point.x
+        selectionViewConstraintY.constant = point.y
     }
 }
