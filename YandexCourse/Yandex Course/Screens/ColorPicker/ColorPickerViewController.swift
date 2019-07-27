12810c79e8e2631e96385543c8ce1d50e46efbc2
Vitaliy Pyatnikov <vpyatnikov@gmail.com>
2019-07-27 17:39:57 +0300

    add: Colors to colorImageView

--- YandexCourse/Yandex Course/Screens/ColorPicker/ColorPickerViewController.swift
+++ YandexCourse/Yandex Course/Screens/ColorPicker/ColorPickerViewController.swift
@@ -30,5 +30,42 @@ final class ColorPickerViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        colorizeImageView()
+    }
+
+    // MARK: - Private
+    /// Identifies spectre lines direction
+    private var hueHorizontal = true
+
+    // MARK: - Color Image view
+
+    private func colorizeImageView() {
+        let size = colorImageView.bounds.size
+        colorImageView.image = foregroundImage(with: size)
+    }
+
+    /// Colorize image view
+    private func foregroundImage(with size: CGSize) -> UIImage {
+        let intWidth = Int(size.width)
+        let intHeight = Int(size.height)
+        var imageData = [UInt8](repeating: 1, count: (4 * intWidth * intHeight))
+        for i in 0 ..< intWidth {
+            for j in 0 ..< intHeight {
+                let index = 4 * (i + j * intWidth)
+                let (hue, saturation) = hueAndSaturation(at: CGPoint(x: i, y: j), size: size) // rendering image transforms it as it it was mirrored around x = -y axis - adjusting for it by switching i and j here
+                let (r, g, b) = UIColor.rgbFrom(hue: hue, saturation: saturation, brightness: 1)
+                imageData[index] = UIColor.getUInt8Value(fromColorComponent: r)
+                imageData[index + 1] = UIColor.getUInt8Value(fromColorComponent: g)
+                imageData[index + 2] = UIColor.getUInt8Value(fromColorComponent: b)
+                imageData[index + 3] = 255
+            }
+        }
+        return UIImage(rgbaBytes: imageData, width: intWidth, height: intHeight) ?? UIImage()
+    }
+    /// Get hue color
+    private func hueAndSaturation(at point: CGPoint, size: CGSize) -> (hue: CGFloat, saturation: CGFloat) {
+        let hue = hueHorizontal ? point.x / size.width : point.y / size.height
+        let saturation = hueHorizontal ? point.y / size.height : point.x / size.width
+        return (max (0, min(1, hue)), 1 - max(0, min(1, saturation)))
     }
 }
