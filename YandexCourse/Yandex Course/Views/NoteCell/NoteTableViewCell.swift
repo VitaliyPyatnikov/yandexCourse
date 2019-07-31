8b6c64e9b52a32623af80fb05fca4feee0404303
Vitaliy Pyatnikov <vpyatnikov@gmail.com>
2019-07-31 10:47:23 +0300

    rea: Add marks

--- YandexCourse/Yandex Course/Views/NoteCell/NoteTableViewCell.swift
+++ YandexCourse/Yandex Course/Views/NoteCell/NoteTableViewCell.swift
@@ -18,14 +18,17 @@ final class NoteTableViewCell: UITableViewCell {
     @IBOutlet weak var noteTitleLabel: UILabel!
     @IBOutlet weak var noteDescriptionLabel: UILabel!
 
+    // MARK: - Life cycle
+
     override func awakeFromNib() {
         super.awakeFromNib()
     }
+
+    // MARK: - Internal
 
     func setup(with model: NoteCellModel) {
         noteColorView.backgroundColor = model.color
         noteTitleLabel.text = model.title
         noteDescriptionLabel.text = model.description
     }
-    
 }
