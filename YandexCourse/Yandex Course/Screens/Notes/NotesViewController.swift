2970e580de28c4fd6b494280f1a8cf7b4effddf8
Vitaliy Pyatnikov <vpyatnikov@gmail.com>
2019-07-31 10:47:16 +0300

    add: Ability to delete notes from table view

--- YandexCourse/Yandex Course/Screens/Notes/NotesViewController.swift
+++ YandexCourse/Yandex Course/Screens/Notes/NotesViewController.swift
@@ -8,6 +8,13 @@
 
 import UIKit
 
+// MARK: - TableViewButtonMode
+
+private enum TableViewButtonMode {
+    case edit
+    case done
+}
+
 // MARK: - NotesWorker
 
 protocol NotesWorker: class {
@@ -45,28 +52,43 @@ final class NotesViewController: UIViewController {
     // MARK: - Private
 
     private let fileNotebook: FileNotebookHandler = FileNotebook()
-    private let editBarButton = UIBarButtonItem(barButtonSystemItem: .edit,
-                                                target: self,
-                                                action: #selector(editMode))
-    private let doneBarButton = UIBarButtonItem(barButtonSystemItem: .done,
-                                                target: self,
-                                                action: #selector(editMode))
 
     private func setupNavigationBar() {
-        navigationItem.leftBarButtonItem = editBarButton
+        setupLeftBarButtonItem(to: .edit)
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add,
                                                             target: self,
                                                             action: #selector(addNewNote))
     }
+    private func setupLeftBarButtonItem(to mode: TableViewButtonMode) {
+        let leftBarButton: UIBarButtonItem
+        switch mode {
+        case .edit:
+            leftBarButton = UIBarButtonItem(barButtonSystemItem: .edit,
+                                             target: self,
+                                             action: #selector(editMode))
+        case .done:
+            leftBarButton = UIBarButtonItem(barButtonSystemItem: .done,
+                                             target: self,
+                                             action: #selector(editMode))
+        }
+        navigationItem.leftBarButtonItem = leftBarButton
+    }
     @objc private func addNewNote() {
         performSegue(withIdentifier: "EditNoteSegue", sender: self)
     }
+    private func removeNote(at indexPath: IndexPath) {
+        let uid = fileNotebook.notes[indexPath.row].uid
+        fileNotebook.remove(with: uid)
+        // TODO: - Check the problem with constrains while deleting cell
+        tableVIew.deleteRows(at: [indexPath], with: .middle)
+    }
     @objc private func editMode() {
-        tableVIew.isEditing.toggle()
         if tableVIew.isEditing {
-            navigationItem.leftBarButtonItem = doneBarButton
+            tableVIew.setEditing(false, animated: true)
+            setupLeftBarButtonItem(to: .edit)
         } else {
-            navigationItem.leftBarButtonItem = editBarButton
+            tableVIew.setEditing(true, animated: true)
+            setupLeftBarButtonItem(to: .done)
         }
     }
     private func setupTableView() {
@@ -77,7 +99,6 @@ final class NotesViewController: UIViewController {
         let nib = UINib(nibName: "NoteTableViewCell", bundle: nil)
         tableVIew.register(nib, forCellReuseIdentifier: "NoteCell")
     }
-
 }
 
 // MARK: - UITableViewDataSource
@@ -98,12 +119,24 @@ extension NotesViewController: UITableViewDataSource {
 
         return cell
     }
+    func tableView(_ tableView: UITableView,
+                   editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
 }
 
 // MARK: - UITableViewDelegate
 
 extension NotesViewController: UITableViewDelegate {
-
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle,
+                   forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            removeNote(at: indexPath)
+        }
+    }
 }
 
 // MARK: - NotesWorker
