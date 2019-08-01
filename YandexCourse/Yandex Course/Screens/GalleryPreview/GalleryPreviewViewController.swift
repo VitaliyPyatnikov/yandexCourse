2d3f8cbeb5763e34fd12a321ceaf92f767899566
Vitaliy Pyatnikov <vpyatnikov@gmail.com>
2019-08-01 23:37:42 +0300

    add: GalleryPreviewViewController

---
+++ YandexCourse/Yandex Course/Screens/GalleryPreview/GalleryPreviewViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GalleryPreviewViewController.swift
+//  Yandex Course
+//
+//  Created by Vitaliy Pyatnikov on 01.08.2019.
+//  Copyright © 2019 Vitaliy Pyatnikov. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - GalleryPreviewViewController
+
+final class GalleryPreviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
