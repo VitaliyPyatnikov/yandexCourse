dc102f31ee3db81e84c49bcf5407b16369753d71
Vitaliy Pyatnikov <vpyatnikov@gmail.com>
2019-08-01 23:00:12 +0300

    add: Basic realisation of gallery collection view

--- YandexCourse/Yandex Course/Screens/Gallery/GalleryViewController.swift
+++ YandexCourse/Yandex Course/Screens/Gallery/GalleryViewController.swift
@@ -8,23 +8,77 @@
 
 import UIKit
 
-class GalleryViewController: UIViewController {
+// MARK: - GalleryViewController
+
+final class GalleryViewController: UIViewController {
+
+    // MARK: - IBOutlets
+
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    // MARK: - Life cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupCollectionView()
     }
-    
 
-    /*
-    // MARK: - Navigation
+    // MARK: - Private
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private let images: [UIImage] = [UIImage]()
+    private let inset: CGFloat = 0.5
+    private let spaceBetweenCels: CGFloat = 8
+    private let cellsPerRow = 4
+
+    private func setupCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        let nib = UINib(nibName: "GalleryCollectionViewCell", bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: "GalleryCollectionViewCell")
     }
-    */
+}
 
+// MARK: - UICollectionViewDataSource
+
+extension GalleryViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GalleryCollectionViewCell", for: indexPath) as? GalleryCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.setup(with: images[indexPath.row])
+
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension GalleryViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: inset, left: inset, bottom: inset, right: inset)
+    }
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return spaceBetweenCels
+    }
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return spaceBetweenCels
+    }
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let safeAreaInsetsSum = CGFloat(0)
+        let marginsAndInsets = inset * 2 + safeAreaInsetsSum + spaceBetweenCels * CGFloat(cellsPerRow - 1)
+        let itemWidth = ((collectionView.bounds.size.width - marginsAndInsets) / CGFloat(cellsPerRow))
+        return CGSize(width: itemWidth, height: itemWidth)
+    }
 }
