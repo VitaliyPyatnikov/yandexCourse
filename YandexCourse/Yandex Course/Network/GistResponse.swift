851be349fdba06f1ca87f0bd982f6371c63a349d
Vitaliy Pyatnikov <vpyatnikov@gmail.com>
2019-08-11 23:49:50 +0300

    add: GistResponse to the project

---
+++ YandexCourse/Yandex Course/Network/GistResponse.swift
@@ -0,0 +1,16 @@
+//
+//  GistResponse.swift
+//  Yandex Course
+//
+//  Created by Vitaliy Pyatnikov on 11.08.2019.
+//  Copyright © 2019 Vitaliy Pyatnikov. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - GistResponse
+
+struct GistResponse: Decodable {
+    let id: String
+}
+
